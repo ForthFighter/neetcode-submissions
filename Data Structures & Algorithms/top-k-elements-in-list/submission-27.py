714597a4f2b642f0cd84fd1939f4d9eac8b3a40f
ForthFighter <gavrielharris@outlook.com>
2026-08-27 15:28:38 +0100

    Add: top-k-elements-in-list - submission-27

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-27.py
@@ -0,0 +1,50 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        count = Counter(nums) # so have a dict with keys as nums, values 
+        # as instances
+        result = []
+        entries_filled = 0
+
+        top_k_sorted_amounts = sorted(list(count.values()))[-k :]
+
+        for num in count.keys():
+            if count[num] in top_k_sorted_amounts:
+                result.append(num)
+
+        return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # Keep order but not duplicates, and only the k maxima
+        # Problem as can have a duplicate amount not for duplicate entry
+
+        sorted_amounts = list(dict.fromkeys(sorted_amounts))
+
+        result = []
+        entries_filled = 0
+
+        
+        for amount in sorted_amounts[:: -1]:
+            if hashmap[amount] not in result:
+                result.append(hashmap[amount])
+                entries_filled += 1
+            if entries_filled == k: return result
+        return result
+
+    
+
+
+
+        
