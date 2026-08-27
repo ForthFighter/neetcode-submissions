9e32b04afa90d1034c0b4412956af09becb14c90
ForthFighter <gavrielharris@outlook.com>
2026-08-27 02:59:41 +0100

    Add: is-anagram - submission-27

---
+++ Data Structures & Algorithms/is-anagram/submission-27.py
@@ -0,0 +1,18 @@
+# NeetCode Solution for O(1) memory
+
+# Idea is to sort them and then check
+# Sorting is O(nlogn) time, though, and can be O(1) memory
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        # Python's built-in sort function is 'sorted'
+
+        return sorted(s) == sorted(t)
+
+        # May have to write own sorting function in the future
+
+            
+
+        
+        
