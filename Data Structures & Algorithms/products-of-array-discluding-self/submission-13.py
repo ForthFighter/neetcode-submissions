1edf6bc36827fd97ced553240e2424f13be32cf8
ForthFighter <gavrielharris@outlook.com>
2026-08-29 00:51:47 +0100

    Add: products-of-array-discluding-self - submission-13

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-13.py
@@ -0,0 +1,35 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        if 0 in nums:
+            if nums.count(0) > 1:
+                return [0]*len(nums)
+            product_without_zero = 1
+            for num in nums:
+                if num != 0:
+                    product_without_zero *= num
+            
+            result = nums[:] # Bad practiceto set a = b to get a copy of b, since then b is just a reference to a, and mutating one affects the other. Can also use result = nums.copy()
+            
+            for i in range(len(result)):
+                if result[i] == 0:
+                    result[i] = product_without_zero
+                else: result[i] = 0
+            return result
+
+        product_all = 1
+        for num in nums:
+            product_all *= num
+        
+        result = [int(product_all / num) for num in nums]
+
+        return result
+
+        # Problem for 0s
+
+        # Also, multiple zeroes
+
+        # if num = 0
+
+        
+             
+        
