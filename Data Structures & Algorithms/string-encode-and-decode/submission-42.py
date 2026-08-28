fafce07b45c462312114a337bd2a101e6a2578c0
ForthFighter <gavrielharris@outlook.com>
2026-08-29 00:22:20 +0100

    Add: string-encode-and-decode - submission-42

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-42.py
@@ -0,0 +1,43 @@
+# Neetcode solution, but coded up my own way - i.e. probably quite poorly
+# Illustrative how tricky this was even with the solution known
+
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+
+        result = ''
+        for string in strs:
+            result += (f'{len(string)}'+'#' + string)
+        return result
+
+    def decode(self, s: str) -> List[str]:
+        result = []
+        current_index = 0
+
+        while current_index < len(s):
+            word_length = ''
+            while True:
+                if s[current_index] == '#':
+                    current_index += 1
+                    break
+                word_length += s[current_index]
+                if current_index == len(s) - 1:
+                    break
+                current_index += 1
+
+            word_length = int(word_length)
+
+            result.append(s[current_index: current_index + word_length])
+            current_index += word_length
+
+        return result
+            
+            
+
+
+
+
+            
+                
+            
