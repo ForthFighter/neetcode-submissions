5edbcfc7e2a774678fa9bd65ab1900f743071de0
ForthFighter <gavrielharris@outlook.com>
2026-09-01 02:06:20 +0100

    Add: three-integer-sum - submission-22

---
+++ Data Structures & Algorithms/three-integer-sum/submission-22.py
@@ -0,0 +1,40 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # Can I reduce this to two sets of twosum?
+
+        n = len(nums)
+
+        hashmap = { nums[i]:i for i in range(n)}
+
+        triplets = []
+
+        for i in range(n):
+            for j in range(n):
+                if -(nums[i] + nums[j]) in hashmap and i != j and i != hashmap[-nums[i]-nums[j]] and j != hashmap[-nums[i]-nums[j]]:
+                    triplets.append(sorted([nums[i],nums[j],-(nums[i]+nums[j])]))
+
+        unique_triplets = []
+
+        # Search for triples.
+
+        cnt = Counter(nums)
+
+        if 0 in cnt and cnt[0] >= 3:
+                unique_triplets.append([0,0,0])
+
+        for num in cnt:
+            if cnt[num] >= 2 and -2*num in hashmap and num != 0:
+                unique_triplets.append(sorted([num,num,-2*num]))
+
+
+        for t in triplets:
+            if t not in unique_triplets and not (t[0] == t[1] or t[0] == t[2] or t[0] == t[1]) :
+                unique_triplets.append(t)
+        return unique_triplets
+        
+
+
+        
+
+        
+        
